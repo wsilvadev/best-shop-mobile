6d9a1c8813abfb2f60a7679f0a8ac06dd9ec8df9
Willian <wsilvadev@gmail.com>
2020-05-10 15:06:06 -0300

    feat(__mocks__): create shopItem

---
+++ src/api/__mocks__/home.ts
@@ -0,0 +1,13 @@
+export const getShops = jest.fn();
+
+export const shopItem = [
+  {
+    id: 1,
+    name: 'Camiseta Primo Tipper',
+    description: 'Rip Curl',
+    image:
+      'https://t-static.dafiti.com.br/weG0u9eKZ4KBV-G0XFOQ5hoY4eI=/fit-in/427x620/dafitistatic-a.akamaihd.net%2fp%2frip-curl-camiseta-rip-curl-primo-tipper-preto-8138-3441052-1-product.jpg',
+    value: '39,99',
+    user_id: '069eeff3a90a8d85',
+  },
+];
